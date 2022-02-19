03274f1dceb982ff3ef536b1c93d052b794c473c
Hozefa Mangalorewala <hozefam@live.in>
2022-02-19 13:38:11 +0300

    Added frontend UI

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import Dashboard from './pages/Dashboard';
+import Header from './components/Header';
+import Login from './pages/Login';
+import React from 'react';
+import Register from './pages/Register';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
